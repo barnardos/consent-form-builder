8b81f79dbe1cfc0f06504c60b9dbbb489b855511
Zac Tolley <zac@thetolleys.com>
2017-07-14 12:02:19 +0100

    Add mocha for testing javascript code
    Added dependencies to bring in mocha js alongside chai and sinon, for providing
    assertions and mocking capabilities. JSDom is used to provide a simulated DOM
    environment in pure js.
    
    The default setup wires them up, sets the required mocha options and adds a helper
    to render markup, parse it through JSDom and return a document object that simulates
    a webpage.

---
+++ spec/javascript/common.js
@@ -0,0 +1,21 @@
+const chai = require('chai')
+const sinon = require('sinon')
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
+
+chai.use(require('sinon-chai'))
+chai.use(require('chai-as-promised'))
+
+// mocha globals
+global.expect = chai.expect
+global.sinon = sinon
+
+chai.config.truncateThreshold = 0
+
+process.setMaxListeners(0)
+process.stdout.setMaxListeners(0)
+
+global.render = function (markup) {
+  const { document } = (new JSDOM(markup).window)
+  return document
+}
