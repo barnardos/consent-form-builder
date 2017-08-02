fdc8bc86e751d6db158c9193876d11c9833d37f6
Russell Garner <rgarner@zephyros-systems.co.uk>
2017-08-02 08:58:04 +0100

    Rubocop: fix hash formatting for Age

--- app/models/age.rb
+++ app/models/age.rb
@@ -1,10 +1,10 @@
 class Age
-  NAME_VALUES = HashWithIndifferentAccess.new({
+  NAME_VALUES = HashWithIndifferentAccess.new(
     under12: 'Under 12 years old',
     '12to18' => '12 to 18 years old',
     over18dr: 'Over 18 years old but unable to sufficiently understand the consent process',
-    over18: 'Over 18 years old',
-  })
+    over18: 'Over 18 years old'
+  )
 
   def self.allowed_values
     NAME_VALUES.keys
