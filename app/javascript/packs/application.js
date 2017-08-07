d5fe47fc185cdc43c76a4417e211e9619519b09e
Zac Tolley <zac@thetolleys.com>
2017-08-07 11:46:27 +0100

    Warn user when exiting page if changes made
    Some, but not all, browsers warn the user if they try to leave a page
    that has changes made to form values unless they submit the form. This
    small change polyfills older browsers to also do this.
    
    The reason the event is added and taken away is that if we just try to
    return false or something from that event handler it puts that on the
    screen. The easiest way to handle things is remove the even handler
    altogether when you submit a page as it's no longer needed.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+require('../lib/exit_warn')
 const HighlightControl = require('../components/highlightcontrol')
 
 HighlightControl.init()
