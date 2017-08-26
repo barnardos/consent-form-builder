81f967e0b1bfd2e48b827a45d9aa109559721d27
Zac Tolley <zac@thetolleys.com>
2017-08-26 22:00:40 +0100

    Add fields for session details
    The new session details page asks for information relating to when the session will take place and equipment required.
    
    Update the schema with the new fields.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170825214510) do
+ActiveRecord::Schema.define(version: 20170826103755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,9 @@ ActiveRecord::Schema.define(version: 20170825214510) do
     t.string "shared_with"
     t.string "shared_duration"
     t.text "shared_use"
+    t.datetime "start_datetime"
+    t.string "duration"
+    t.text "participant_equipment"
     t.index ["status"], name: "index_research_sessions_on_status"
   end
 
