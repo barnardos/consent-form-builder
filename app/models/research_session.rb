b19af781e0c73c4a8c6a65d9f65bc100dcfeacad
Zac Tolley <zac@thetolleys.com>
2017-08-29 14:31:19 +0100

    Re-order pages
    Change the order of pages so it is now:
    * Researcher
    * Topic
    * Purpose
    * Methodologies
    * Recording
    * Data
    * Time and Equipment
    * Incentive
    * Preview

--- app/models/research_session.rb
+++ app/models/research_session.rb
@@ -1,24 +1,4 @@
 class ResearchSession < ApplicationRecord
-  validates :methodologies,
-            has_at_least_one: { of: Methodologies.allowed_values },
-            if: -> (session) { session.reached_step?(:methodologies) }
-  validates :other_methodology, presence: true,
-            if: lambda { |session|
-              session.reached_step?(:methodologies) &&
-                session.methodologies&.any? { |item| item.to_s == 'other' }
-            }
-  validates :other_recording_method, presence: true,
-            if: lambda { |session|
-              session.reached_step?(:recording) &&
-                session.recording_methods&.any? { |item| item.to_s == 'other' }
-            }
-  validates :recording_methods,
-            has_at_least_one: { of: RecordingMethods.allowed_values },
-            if: -> (session) { session.reached_step?(:recording) }
-  validates :topic, presence: true,
-            if: -> (session) { session.reached_step?(:topic) }
-  validates :purpose, presence: true,
-            if: -> (session) { session.reached_step?(:purpose) }
   validates :researcher_name, presence: true,
             if: -> (session) { session.reached_step?(:researcher) }
   validates :researcher_phone, presence: true,
@@ -27,6 +7,30 @@ class ResearchSession < ApplicationRecord
             if: -> (session) { session.reached_step?(:researcher) }
   validates :researcher_email, format: /@/,
     if: -> (session) { session.researcher_email.present? && session.reached_step?(:researcher) }
+
+  validates :topic, presence: true,
+    if: -> (session) { session.reached_step?(:topic) }
+
+  validates :purpose, presence: true,
+    if: -> (session) { session.reached_step?(:purpose) }
+
+  validates :methodologies,
+            has_at_least_one: { of: Methodologies.allowed_values },
+            if: -> (session) { session.reached_step?(:methodologies) }
+  validates :other_methodology, presence: true,
+            if: lambda { |session|
+              session.reached_step?(:methodologies) &&
+                session.methodologies&.any? { |item| item.to_s == 'other' }
+            }
+
+  validates :other_recording_method, presence: true,
+            if: lambda { |session|
+              session.reached_step?(:recording) &&
+                session.recording_methods&.any? { |item| item.to_s == 'other' }
+            }
+  validates :recording_methods,
+            has_at_least_one: { of: RecordingMethods.allowed_values },
+            if: -> (session) { session.reached_step?(:recording) }
 
   validates :shared_with, inclusion: { in: SharedWith.allowed_values },
             if: -> (session) { session.reached_step?(:data) }
@@ -38,9 +42,9 @@ class ResearchSession < ApplicationRecord
             if: -> (session) { session.reached_step?(:data) }
 
   validates :payment_type, inclusion: { in: PaymentType.allowed_values },
-    if: -> (session) { session.incentive && session.reached_step?(:incentive) }
+            if: -> (session) { session.incentive && session.reached_step?(:incentive) }
   validates :incentive_value, presence: true,
-    if: -> (session) { session.incentive && session.reached_step?(:incentive) }
+            if: -> (session) { session.incentive && session.reached_step?(:incentive) }
 
   def able_to_consent?
     age == 'over18'
