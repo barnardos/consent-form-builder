20392d2e5551fe7bcedab3225a13e0041d5843ba
Russell Garner <rgarner@zephyros-systems.co.uk>
2017-09-14 15:02:03 +0100

    Remove a mendacious routes.rb comment
    It was no longer true

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'research_sessions#index'
 
-  # NB: the below is not totally idiomatic Rails routing, but I wanted
-  # to ape the express.js version as closely as possible by way of example.
-  # Usually you might say # resources :research_sessions, only: [:show, :create],
-  # but the created routes/verbs # don't match the JS completely.
-  # So I've gone for get = #show and post = #create, which is close to normal
-  # +resources+ semantics.
   get '/start', to: 'research_sessions#start'
 
   resources :questions, only: [:show, :update], controller: 'research_sessions'
