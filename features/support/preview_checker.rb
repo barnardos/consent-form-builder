a7abee586459dd807eefd6bd80abec4dc481dae8
Russell Garner <rgarner@zephyros-systems.co.uk>
2017-09-25 14:31:53 +0100

    Add null check methods for preview
    * Nothing is implemented
    * It says so as it goes
    * We mix in check_<step> methods for Cucumber for every wizard step

---
+++ features/support/preview_checker.rb
@@ -0,0 +1,39 @@
+module PreviewChecker
+  def check_researcher
+    puts "#{__method__} not implemented"
+  end
+
+  def check_topic
+    puts "#{__method__} not implemented"
+  end
+
+  def check_purpose
+    puts "#{__method__} not implemented"
+  end
+
+  def check_methodologies
+    puts "#{__method__} not implemented"
+  end
+
+  def check_recording
+    puts "#{__method__} not implemented"
+  end
+
+  def check_data
+    puts "#{__method__} not implemented"
+  end
+
+  def check_time_equipment
+    puts "#{__method__} not implemented"
+  end
+
+  def check_expenses
+    puts "#{__method__} not implemented"
+  end
+
+  def check_incentive
+    puts "#{__method__} not implemented"
+  end
+end
+
+World PreviewChecker
