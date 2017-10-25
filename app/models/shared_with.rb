831f024715b93cd7271419e509b01765c27ef368
Ida Rolek <ida@immedio.co.uk>
2017-10-25 16:35:29 +0100

    Internationalise SharedWith labels
    As part of our drive to move more text to en.yml, make the individual
    labels of SharedWith::NAME_VALUES part of en.yml

--- app/models/shared_with.rb
+++ app/models/shared_with.rb
@@ -1,9 +1,9 @@
 class SharedWith
   NAME_VALUES = HashWithIndifferentAccess.new(
-    anonymised: 'All identifiable information will be anonymised as we process it',
-    team:       'Identifiable information will only be shared with the research team at Barnardo\'s',
-    internal:   'Identifiable information will be shared with other teams in Barnardo\'s',
-    external:   'Identifiable information will be used in external publications'
+    anonymised: I18n.t('helpers.label.storing_information.shared_with.anonymised'),
+    team:       I18n.t('helpers.label.storing_information.shared_with.team'),
+    internal:   I18n.t('helpers.label.storing_information.shared_with.internal'),
+    external:   I18n.t('helpers.label.storing_information.shared_with.xternal')
   )
 
   def self.allowed_values
