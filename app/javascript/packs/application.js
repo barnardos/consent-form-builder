c927b72b683ee41dab7e44635fc27e99a869d272
Russell Garner <rgarner@zephyros-systems.co.uk>
2017-11-13 08:59:02 +0000

    rails generate react:install

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,3 +19,7 @@ HighlightControl.init()
 ConditionalSubfields.init()
 PrintSection.init()
 Share.init()
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true)
+var ReactRailsUJS = require("react_ujs")
+ReactRailsUJS.useContext(componentRequireContext)
