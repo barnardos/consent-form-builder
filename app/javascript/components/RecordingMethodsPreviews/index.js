f7b85a0f9f978e8ce18b2e1ed7a31b46f0db61b0
Russell Garner <rgarner@zephyros-systems.co.uk>
2017-12-14 10:43:19 +0000

    Correct recording/live preview heading classes
    * Should be reactive-preview__heading

--- app/javascript/components/RecordingMethodsPreviews/index.js
+++ app/javascript/components/RecordingMethodsPreviews/index.js
@@ -13,13 +13,13 @@ class RecordingMethodsPreviews extends PreviewBase {
     return (
       <div className="reactive-container__preview">
         <section className='reactive-preview__section'>
-          <h3 className="subtitle-small" id="what">What happens in this research session?</h3>
+          <h3 className="reactive-preview__heading" id="what">What happens in this research session?</h3>
           <p>&hellip;</p>
           <WhichWeWillRecordUsing {...this.state } />
           <p>&hellip;</p>
         </section>
         <section className='reactive-preview__section'>
-          <h3 className="subtitle-small" id="what">Consent Form</h3>
+          <h3 className="reactive-preview__heading" id="consent-form">Consent Form</h3>
           <ul className="bullet-point-list">
             <li>&hellip;</li>
             <WhichWeWillRecordUsingSentence {...this.state} />
