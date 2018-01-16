3b4961b74f6d7f58b959ff993700a506422696d3
Elaine Osbourn <elaine.osbourn@MacBook-Pro-CI064210.local>
2018-01-16 15:09:57 +0000

    Create 404/500 pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   helper Barnardos::ActionView::FormHelper
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+
   protect_from_forgery with: :exception
+
+  def render_404
+    redirect_to '/404'
+  end
 end
