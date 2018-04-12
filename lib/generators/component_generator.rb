3b952ff2064e6f61045eb89c32afa35996a7a4de
Joel Carr <joel.ben.carr@gmail.com>
2018-04-12 16:31:04 +0100

    Create a rails generator to create a new component
    Make a rails generator to create a new component using the command
    `rails g component `ComponentName`.

---
+++ lib/generators/component_generator.rb
@@ -0,0 +1,26 @@
+class ComponentGenerator < Rails::Generators::Base
+  argument :component_name, required: true, desc: 'Component name, e.g: Breadcrumbs'
+
+  def create_view_file
+    create_file "#{component_path}/_index.html.erb"
+  end
+
+  def create_css_file
+    create_file "#{component_path}/index.css" do
+      "/** @define #{component_name}; */\n"
+    end
+  end
+
+  def create_js_file
+    create_file "#{component_path}/index.js" do
+      # require component's CSS inside JS automatically
+      "import \"./index.css\";\n"
+    end
+  end
+
+protected
+
+  def component_path
+    "frontend/components/#{component_name}"
+  end
+end
