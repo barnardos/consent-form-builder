092b1eb3009332bbd0d4a4996a187f80dc91714b
Joel Carr <joel@joelcarr.co.uk>
2018-04-24 16:16:03 +0100

    Layout components with proportional spacing
    Create Step component to layout components within and space components
    within the Form component.

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -2,6 +2,7 @@ import "init";
 
 import "components/Form";
 import "components/Layout";
+import "components/Step";
 import "components/Title";
 import "components/SinglelineTextControl";
 import "components/Submit";
