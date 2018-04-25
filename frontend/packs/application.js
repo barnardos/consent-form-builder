22aaabe26e85898e83b1c920343cba054decc1aa
Joel Carr <joel@joelcarr.co.uk>
2018-04-25 16:07:35 +0100

    Add Lede component to enable lead paragraphs

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -3,6 +3,7 @@ import "init";
 import "components/ErrorSummary";
 import "components/Form";
 import "components/Layout";
+import "components/Lede";
 import "components/Step";
 import "components/Title";
 import "components/SinglelineTextControl";
