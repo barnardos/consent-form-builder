7ab4aabcda77b7a76a5379762410ff9782f903b4
kittysquee <kittysquee@gmail.com>
2018-05-23 15:27:31 +0100

    Refactor validator

--- app/validators/has_at_least_one_validator.rb
+++ app/validators/has_at_least_one_validator.rb
@@ -9,25 +9,42 @@ class HasAtLeastOneValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, values)
     valid_values = options[:of]
 
-    if values.blank?
-      record.errors[attribute] << 'should have at least one of '\
-        "#{valid_values.to_sentence(last_word_connector: ', or ')}"
-      return
-    end
+    return if no_values(record, attribute, values, valid_values)
 
-    unless values.respond_to?(:each)
-      record.errors[attribute] << 'should be an enumerable'
-      return
-    end
+    return unless enumerable?(record, attribute, values)
 
     invalid_values_found = values.each_with_object([]) do |value, found|
       found << value.to_s.humanize(capitalize: false) unless valid_values.include?(value.to_sym)
     end
 
-    if invalid_values_found.any?
-      record.errors[attribute] << \
-        'has these invalid values: '\
-        "#{invalid_values_found.to_sentence}"
+    set_invalid_values(record, attribute, invalid_values_found)
+  end
+
+  private
+
+  def no_values(record, attribute, values, valid_values)
+    if values.blank?
+      record.errors[attribute] << 'should have at least one of '\
+        "#{valid_values.to_sentence(last_word_connector: ', or ')}"
+      true
+    else
+      false
     end
+  end
+
+  def enumerable?(record, attribute, values)
+    if values.respond_to?(:each)
+      true
+    else
+      record.errors[attribute] << 'should be an enumerable'
+      false
+    end
+  end
+
+  def set_invalid_values(record, attribute, invalid_values_found)
+    return if invalid_values_found.empty?
+    record.errors[attribute] << \
+      'has these invalid values: '\
+      "#{invalid_values_found.to_sentence}"
   end
 end
